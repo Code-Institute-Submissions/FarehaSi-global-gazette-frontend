c185c7dd028c18f64dbe2dcbead39e84f6559474
Fareha SI <fareha.s@live.com>
2023-11-01 19:10:50 +0400

    created boiler plate for reusable API and Routing

---
+++ src/data/routes.js
@@ -0,0 +1,13 @@
+import Home from "../pages/Home";
+
+
+const routes = [
+    {
+        id: 1, 
+        path: '/',
+        element: Home,
+        isProtected: false
+    }
+];
+
+export default routes;
