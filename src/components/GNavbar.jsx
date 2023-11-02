65171630b04b6e9c4dd6d546609916c151cf4541
Fareha SI <fareha.s@live.com>
2023-11-02 04:40:53 +0400

    completed the guest navbar

---
+++ src/components/GNavbar.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import worldLogo from '../assets/img/earth.png';
+
+const GNavbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg px-4 navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        <img
+          src={worldLogo}
+          width="30"
+          height="30"
+          alt="Global Gazette Logo"
+          className="d-inline-block align-top"
+        />
+        Global Gazette
+      </a>
+
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#mission">Our Mission & Community</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#signup">Sign Up</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#signin">Sign In</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default GNavbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import GNavbar from '../components/GNavbar'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <GNavbar />
+    </div>
   )
 }
 
