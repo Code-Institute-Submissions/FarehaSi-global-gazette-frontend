3433de91222dfbf4439dda4bd03cbecd3397cf9a
Fareha SI <fareha.s@live.com>
2023-11-06 10:56:45 +0400

    completed comment reply

--- src/components/articles/comments/SingleComment.jsx
+++ src/components/articles/comments/SingleComment.jsx
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SingleComment.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import apiFetch from '../../../utils/api';
 
 const SingleComment = ({ comment }) => {
+  const [likeCount, setLikeCount] = useState(comment.like_count);
+  const [liked, setLiked] = useState(false);
+
   const avatarUrl = comment.userImage || `https://api.multiavatar.com/${encodeURIComponent(comment.username)}.svg`;
+
+  const handleLike = async () => {
+    try {
+      const response = await apiFetch(`/comments/${comment.id}/react/like/`, { method: 'POST' });
+
+      if (response.detail === "Reaction 'like' set.") {
+        setLikeCount(prevCount => prevCount + 1); 
+        setLiked(true);
+      } else if (response.detail === "Reaction 'like' removed.") {
+        setLikeCount(prevCount => prevCount - 1);
+        setLiked(false);
+      }
+    } catch (error) {
+      console.error('Error liking the comment:', error);
+    }
+  };
+
+  const renderReplies = (replies) => {
+    return replies.map(reply => (
+      <div className="media py-3" key={reply.id} style={{ marginLeft: '20px' }}>
+        <SingleComment comment={reply} />
+      </div>
+    ));
+  };
 
   return (
     <div className="media py-3">
@@ -21,14 +49,14 @@ const SingleComment = ({ comment }) => {
         <h5 className="mt-0 mb-1">{comment.username}</h5>
         <p className="mb-2">{comment.text}</p>
         <div className="comment-actions">
-          <span className="comment-like me-2">
-            <FontAwesomeIcon icon={faThumbsUp} /> {comment.like_count}
-          </span>
+          <button className={`btn btn-outline-dark comment-like me-2 ${liked ? 'liked' : ''}`} onClick={handleLike}>
+            <FontAwesomeIcon icon={faThumbsUp} /> {likeCount}
+          </button>
           <span className="comment-reply">
             <FontAwesomeIcon icon={faComment} /> Reply
           </span>
         </div>
-
+        {comment.replies && renderReplies(comment.replies)}
       </div>
     </div>
   );
