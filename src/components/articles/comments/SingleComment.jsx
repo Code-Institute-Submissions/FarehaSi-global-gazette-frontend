8676bee689558f3a96dd125f1a040a51b359d399
Fareha SI <fareha.s@live.com>
2023-11-06 11:30:42 +0400

    comments nesting enabled

--- src/components/articles/comments/SingleComment.jsx
+++ src/components/articles/comments/SingleComment.jsx
@@ -3,10 +3,13 @@ import './SingleComment.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import apiFetch from '../../../utils/api';
+import { faCommentDots } from '@fortawesome/free-regular-svg-icons';
 
 const SingleComment = ({ comment }) => {
-  const [likeCount, setLikeCount] = useState(comment.like_count);
-  const [liked, setLiked] = useState(false);
+    const [likeCount, setLikeCount] = useState(comment.like_count);
+    const [liked, setLiked] = useState(false);
+    const [replyText, setReplyText] = useState('');
+    const [showReply, setShowReply] = useState(false);
 
   const avatarUrl = comment.userImage || `https://api.multiavatar.com/${encodeURIComponent(comment.username)}.svg`;
 
@@ -26,6 +29,34 @@ const SingleComment = ({ comment }) => {
     }
   };
 
+  const handleSubmitReply = async () => {
+    try {
+      const response = await apiFetch(`/comments/${comment.id}/reply/`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          text: replyText,
+          article: comment.article,
+          parent_comment: comment.id
+        })
+      });
+
+      if (response.ok) {
+        const newReply = await response.json();
+        updateComments(newReply);
+        setReplyText('');
+        setShowReply(false);
+      } else {
+        console.error('Failed to submit reply');
+      }
+    } catch (error) {
+      console.error('Error submitting the reply:', error);
+    }
+  };
+
+
   const renderReplies = (replies) => {
     return replies.map(reply => (
       <div className="media py-3" key={reply.id} style={{ marginLeft: '20px' }}>
@@ -38,25 +69,49 @@ const SingleComment = ({ comment }) => {
     <div className="media py-3">
       <div className="media-body">
         <div className="d-flex align-items-center">
-            <img 
-                src={avatarUrl} 
-                alt={`${comment.username}'s Avatar`} 
-                className="mr-3 rounded-circle" 
-                style={{ width: '28px', height: '28px' }} 
-            />
-            <span>Hero</span>
+          <img 
+            src={avatarUrl} 
+            alt={`${comment.username}'s avatar`} 
+            className="mr-3 rounded-circle" 
+            style={{ width: '28px', height: '28px' }} 
+          />
+          <h5 className="mt-0 mb-1">{comment.username}</h5>
         </div>
-        <h5 className="mt-0 mb-1">{comment.username}</h5>
         <p className="mb-2">{comment.text}</p>
         <div className="comment-actions">
           <button className={`btn btn-outline-dark comment-like me-2 ${liked ? 'liked' : ''}`} onClick={handleLike}>
             <FontAwesomeIcon icon={faThumbsUp} /> {likeCount}
           </button>
-          <span className="comment-reply">
-            <FontAwesomeIcon icon={faComment} /> Reply
-          </span>
+          <button className="btn btn-outline-dark comment-reply" onClick={() => setShowReply(!showReply)}>
+            <FontAwesomeIcon icon={faCommentDots} /> Reply
+          </button>
         </div>
-        {comment.replies && renderReplies(comment.replies)}
+        {showReply && (
+          <div className="reply-form">
+            <textarea
+              className="form-control"
+              value={replyText}
+              onChange={(e) => setReplyText(e.target.value)}
+              placeholder="Write a reply..."
+            />
+            <button
+              className="btn btn-primary mt-2"
+              onClick={handleSubmitReply}
+              disabled={!replyText.trim()}
+            >
+              Submit Reply
+            </button>
+            <button
+              className="btn btn-secondary mt-2"
+              onClick={() => setShowReply(false)}
+            >
+              Cancel
+            </button>
+          </div>
+        )}
+        <div className="comment-replies">
+          {comment.replies && renderReplies(comment.replies)}
+        </div>
       </div>
     </div>
   );
